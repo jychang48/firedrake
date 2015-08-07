217bd8d8678ab10adfb18a1d515b0752ea2428a5
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2015-08-07 09:38:59 +0100

    _version: use prefork.call_capture_output rather than Popen
    Avoid calling fork in a potentially crippled MPI process.

--- firedrake/_version.py
+++ firedrake/_version.py
@@ -12,7 +12,7 @@ git_refnames = "$Format:%d$"
 git_full = "$Format:%H$"
 
 
-import subprocess
+from pyop2_utils import prefork
 import sys
 import errno
 import re
@@ -21,18 +21,13 @@ import os.path
 
 def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False):
     assert isinstance(commands, list)
-    p = None
     for c in commands:
         try:
-            # remember shell=False, so use git.cmd on windows, not just git
-            p = subprocess.Popen([c] + args, cwd=cwd, stdout=subprocess.PIPE,
-                                 stderr=(subprocess.PIPE if hide_stderr
-                                         else None))
+            retval, stdout, stderr = prefork.call_capture_output([c] + args,
+                                                                 cwd=cwd,
+                                                                 error_on_nonzero=False)
             break
-        except EnvironmentError:
-            e = sys.exc_info()[1]
-            if e.errno == errno.ENOENT:
-                continue
+        except prefork.ExecError as e:
             if verbose:
                 print("unable to run %s" % args[0])
                 print(e)
@@ -41,10 +36,9 @@ def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False):
         if verbose:
             print("unable to find command, tried %s" % (commands,))
         return None
-    stdout = p.communicate()[0].strip()
     if sys.version >= '3':
         stdout = stdout.decode()
-    if p.returncode != 0:
+    if retval != 0:
         if verbose:
             print("unable to run %s (error)" % args[0])
         return None
