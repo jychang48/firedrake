fb3aab0eebfb1922fc31efe9e19771a95ce87886
David Ham <david.ham@imperial.ac.uk>
2015-09-02 11:43:23 +0100

    and another missing m

--- firedrake/fiat_utils.py
+++ firedrake/fiat_utils.py
@@ -17,7 +17,7 @@ def fiat_from_ufl_element(ufl_element):
     try:
         return _fiat_element_cache[ufl_element]
     except KeyError:
-        fiat_element = ffc.create_actual_fiat_element(ufl_element)
+        fiat_element = mffc.create_actual_fiat_element(ufl_element)
         _fiat_element_cache[ufl_element] = fiat_element
         return fiat_element
 
